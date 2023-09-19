b5ced2f571de7e4d22efe831e1050dee22888a97
Antonio <antoniojosedonishung@gmail.com>
2023-09-19 11:55:44 -0500

    fix: added the model to the user object of the response (#41)

--- router/query.go
+++ router/query.go
@@ -17,6 +17,6 @@ func (r *Router) UserByUsername(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, InternalServerError(err))
 		return
 	}
-	res := models.User{Username: user.Username}
+	res := models.User{Model: user.Model, Username: user.Username}
 	ctx.JSON(http.StatusOK, res)
 }
